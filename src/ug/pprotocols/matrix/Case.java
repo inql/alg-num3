fe46093d96c9d1cb028d04f00f1ac5e50e1904ce
inql <dbinkus96@gmail.com>
2018-12-02 18:35:15 +0100

    refactoring

---
+++ src/ug/pprotocols/matrix/Case.java
@@ -0,0 +1,12 @@
+package ug.pprotocols.matrix;
+
+public class Case {
+
+    int yesVoters;
+    int noVoters;
+
+    public Case(int yesVoters, int noVoters) {
+        this.yesVoters = yesVoters;
+        this.noVoters = noVoters;
+    }
+}
